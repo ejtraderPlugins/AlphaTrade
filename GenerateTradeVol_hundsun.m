bf7064a99f10640b300bcb27848110ed32332998
qiyujian <qiyujian2010@live.com>
2016-05-10 17:35:37 +0800

    Add generate trade vol a8. add n_part, in order to generate n parts trade file. according to xml file tag-name npart.

--- GenerateTradeVol_hundsun.m
+++ GenerateTradeVol_hundsun.m
@@ -12,13 +12,13 @@ end
 [idate, itime] = GetDateTimeNum();
 fprintf(fid_log, '--->>> %s_%s,\t Begin generate trade vol. account = %s.\n', num2str(idate), num2str(itime), AccountInfo{ai}.NAME);
 
+N_PART = str2double(AccountInfo{ai}.NPART);% 要写成N_PART个篮子文件，在xml中设置
 path_account = AccountInfo{ai}.ACCOUNTPATH ;
 
 file_target = [path_account 'target_holding.txt'];
 file_current = [path_account 'current_holding.txt'];
 file_trade = [path_account 'trade_holding.txt'];
 file_modle = [path_account '\modle.xlsx'];
-file_today = [path_current 'trade_p0.xlsx'];
 
 %% load target file
 if exist(file_target, 'file')
@@ -37,7 +37,7 @@ end
 unionTicker = union(tHolding(:,1), cHolding(:,1));
 unionTicker(all(unionTicker == 0, 2), :) = [];
 numOfTicker = size(unionTicker,1);
-unionHolding = zeros(numOfTicker, 3);%第一列是ticker，第二列是target，第三列是current
+unionHolding = zeros(numOfTicker, 4);%第一列是ticker，第二列是target，第三列是current, 第四列是available
 unionHolding(:,1) = unionTicker;
 for i = 1:numOfTicker
     pT = find(tHolding(:,1) == unionHolding(i,1), 1, 'first');
@@ -49,100 +49,143 @@ for i = 1:numOfTicker
     end
     if isempty(pC)
         unionHolding(i,3) = 0;
+		unionHolding(i,4) = 0;
     else
         unionHolding(i,3) = cHolding(pC, 2);
+		unionHolding(i,4) = cHolding(pC, 3);
     end
 end
 diffHolding = [unionHolding(:,1) unionHolding(:,2) - unionHolding(:,3)];
+
 fid = fopen(file_trade, 'w');
 fprintf(fid, [repmat('%15d\t',1,size(diffHolding,2)), '\n'], diffHolding');
 fclose(fid);
+[idate, itime] = GetDateTimeNum();
+fprintf(fid_log, '--->>> %s_%s,\tDONE. Write trade vol file. file = %s.\n', num2str(idate), num2str(itime), file_trade);
 
-fprintf(fid_log, '--->>> %s_%s,\tWrite trade vol file. file = %s.\n', num2str(idate), num2str(itime), file_trade);
 dst_file_trade = [path_account '\HistoricalTrade\trade_holding_' num2str(idate) '_' num2str(itime) '.txt'];
 CopyFile2HistoryDir(file_trade, dst_file_trade);
 
 %% write into trade files for client
+% consider the available holding
+unionHolding(all(diffHolding(:,2) == 0, 2), :) = [];
 diffHolding(all(diffHolding(:,2) == 0,2), :) = [];
+diffHolding(:,2) = min(abs(diffHolding(:,2)), unionHolding(:,3)) .* abs(diffHolding(:,2)) ./ diffHolding(:,2) .* (diffHolding(:,2) < 0)...
+                   + diffHolding(:,2) .* (diffHolding(:,2) > 0);
+diffHolding(all(diffHolding(:,2) == 0,2), :) = [];
+
+% devide into N_PART
 numOfTrade = size(diffHolding,1);
+one = ones(numOfTrade, numOfTrade);
 
-Ticker = cell(numOfTrade, 1);
-Name = cell(numOfTrade, 1);
-Market = zeros(numOfTrade, 1);
-BS = zeros(numOfTrade, 1);
-PriceType = cell(numOfTrade, 1);
-Price = zeros(numOfTrade, 1);
-Vol = zeros(numOfTrade, 1);
-Money = zeros(numOfTrade, 1);
-for i = 1:numOfTrade
-    Ticker{i} = ['''' num2str(diffHolding(i,1), '%06d')];
-    Name{i} = '';
-    if diffHolding(i,1) < 600000
-        Market(i) = 2;
-        PriceType{i} = 'A';
-    else
-        Market(i) = 1;
-        PriceType{i} = 'b';
-    end
-    if diffHolding(i,2) > 0
-        BS(i) = 1;
-    elseif diffHolding(i,2) < 0
-        BS(i) = 2;
-    end
-    Vol(i) = abs(diffHolding(i,2));
+dev_vol = floor(abs(diffHolding(:,2)) / 100 / N_PART);
+rem_vol = rem(floor(abs(diffHolding(:,2) / 100)), N_PART);
+
+dev_vol = diag(dev_vol);
+dev_vol = (one * dev_vol)';
+dev_vol(:,N_PART+1:end) = [];
+
+rem_vol = diag(rem_vol);
+rem_vol = (one * rem_vol)';
+rem_vol(:, N_PART+1:end) = [];
+tmp = 1:N_PART;
+tmp = repmat(tmp, numOfTrade, 1);
+tmp(:, N_PART+1:end) = [];
+rem_vol = ((rem_vol - tmp) >= 0);
+
+bs = abs(diffHolding(:,2)) ./ diffHolding(:,2);
+bs = diag(bs);
+bs = (one * bs)';
+bs(:,N_PART+1:end) = [];
+
+child_vol = (dev_vol + rem_vol) . * bs * 100; % 乘以100后变成股数, 并且带有符号
+
+% begin to write in parts
+for ipart = 1:N_PART
+	file_name = ['trade_p' num2str(ipart)];
+	file_today = [path_account file_name '.xlsx'];
+	
+	Ticker = cell(numOfTrade, 1);
+	Name = cell(numOfTrade, 1);
+	Market = zeros(numOfTrade, 1);
+	BS = zeros(numOfTrade, 1);
+	PriceType = cell(numOfTrade, 1);
+	Price = zeros(numOfTrade, 1);
+	Vol = zeros(numOfTrade, 1);
+	Money = zeros(numOfTrade, 1);
+	
+	for i = 1:numOfTrade
+		Ticker{i} = ['''' num2str(diffHolding(i,1), '%06d')];
+		Name{i} = '';
+		if diffHolding(i,1) < 600000
+			Market(i) = 2;
+			PriceType{i} = 'A';
+		else
+			Market(i) = 1;
+			PriceType{i} = 'b';
+		end
+		if child_vol(i,ipart) > 0
+			BS(i) = 1;
+		elseif child_vol(i,ipart) < 0
+			BS(i) = 2;
+		end
+		Vol(i) = abs(child_vol(i,ipart));
+	end
+	if exist(file_today, 'file')
+		delete(file_today);
+	end
+	if copyfile(file_modle, file_today,'f') == 1
+		if xlswrite(file_today, Ticker, 'SHEET1', 'A2') == 1
+			fprintf('Ticker Done.\n');
+		else
+			fprintf('Tickrer Failed.\n');
+		end
+		if xlswrite(file_today, Name, 'SHEET1', 'B2') == 1
+			fprintf('Name Done.\n');
+		else
+			fprintf('Name Failed.\n');
+		end
+		if xlswrite(file_today, Market, 'SHEET1', 'C2') == 1
+			fprintf('Market Done.\n');
+		else
+			fprintf('Market Failed.\n');
+		end
+		if xlswrite(file_today, BS, 'SHEET1', 'D2') == 1
+			fprintf('BS Done.\n');
+		else
+			fprintf('BS Failed.\n');
+		end
+		if xlswrite(file_today, PriceType, 'SHEET1', 'E2') == 1
+			fprintf('PriceType Done.\n');
+		else
+			fprintf('PriceType Failed.\n');
+		end
+		if xlswrite(file_today, Price, 'SHEET1', 'F2') == 1
+			fprintf('Price Done.\n');
+		else
+			fprintf('Price Failed.\n');
+		end
+		if xlswrite(file_today, Vol, 'SHEET1', 'G2') == 1
+			fprintf('Vol Done.\n');
+		else
+			fprintf('Vol Failed.\n');
+		end
+		if xlswrite(file_today, Money, 'SHEET1', 'H2') == 1
+			fprintf('Money Done.\n');
+		else
+			fprintf('Money Failed.\n');
+		end
+		system('taskkill /f /im excel.exe');
+		
+		[idate, itime] = GetDateTimeNum();
+		fprintf(fid_log, '--->>> %s_%s,\tDone write trade file. file = %s.\n', num2str(idate), num2str(itime), file_today);
+		dst_file_today = [path_account '\HistoricalTrade\' file_name '_' num2str(idate) '_' num2str(itime) '.xlsx'];
+		CopyFile2HistoryDir(file_today, dst_file_today); 
+	else
+		[idate, itime] = GetDateTimeNum();
+		fprintf(fid_log, '--->>> %s_%s,\tError when copy modle file, when generate trade file. account = %s, file = %s.\n', num2str(idate), num2str(itime), AccountInfo{ai}.NAME, file_modle);
+	end
 end
-
-if copyfile(file_modle, file_today,'f') == 1
-    if xlswrite(file_today, Ticker, 'SHEET1', 'A2') == 1
-        fprintf('Ticker Done.\n');
-    else
-        fprintf('Tickrer Failed.\n');
-    end
-    if xlswrite(file_today, Name, 'SHEET1', 'B2') == 1
-        fprintf('Name Done.\n');
-    else
-        fprintf('Name Failed.\n');
-    end
-    if xlswrite(file_today, Market, 'SHEET1', 'C2') == 1
-        fprintf('Market Done.\n');
-    else
-        fprintf('Market Failed.\n');
-    end
-    if xlswrite(file_today, BS, 'SHEET1', 'D2') == 1
-        fprintf('BS Done.\n');
-    else
-        fprintf('BS Failed.\n');
-    end
-    if xlswrite(file_today, PriceType, 'SHEET1', 'E2') == 1
-        fprintf('PriceType Done.\n');
-    else
-        fprintf('PriceType Failed.\n');
-    end
-    if xlswrite(file_today, Price, 'SHEET1', 'F2') == 1
-        fprintf('Price Done.\n');
-    else
-        fprintf('Price Failed.\n');
-    end
-    if xlswrite(file_today, Vol, 'SHEET1', 'G2') == 1
-        fprintf('Vol Done.\n');
-    else
-        fprintf('Vol Failed.\n');
-    end
-    if xlswrite(file_today, Money, 'SHEET1', 'H2') == 1
-        fprintf('Money Done.\n');
-    else
-        fprintf('Money Failed.\n');
-    end
-    system('taskkill /f /im excel.exe');
-else
-    [idate, itime] = GetDateTimeNum();
-    fprintf(fid_log, '--->>> %s_%s,\tError when copy modle file, when generate trade file. account = %s, file = %s.\n', num2str(idate), num2str(itime), AccountInfo{ai}.NAME, file_modle);
-end
-
-[idate, itime] = GetDateTimeNum();
-fprintf(fid_log, '--->>> %s_%s,\tDone write trade file. file = %s.\n', num2str(idate), num2str(itime), file_today);
-dst_file_today = [path_account '\HistoricalTrade\trade_p0_' num2str(idate) '_' num2str(itime) '.txt'];
-CopyFile2HistoryDir(file_today, dst_file_today); 
     
 [idate, itime] = GetDateTimeNum();
 fprintf(fid_log, '--->>> %s_%s,\t End generate trade vol. account = %s.\n', num2str(idate), num2str(itime), AccountInfo{ai}.NAME);
--- GenerateTradeVol_ims.m
+++ GenerateTradeVol_ims.m
@@ -12,12 +12,12 @@ end
 [idate, itime] = GetDateTimeNum();
 fprintf(fid_log, '--->>> %s_%s,\t Begin generate trade vol. account = %s.\n', num2str(idate), num2str(itime), AccountInfo{ai}.NAME);
 
+N_PART = str2double(AccountInfo{ai}.NPART);% 要写成N_PART个篮子文件，在xml中设置
 path_account = AccountInfo{ai}.ACCOUNTPATH ;
 
 file_target = [path_account 'target_holding.txt'];
 file_current = [path_account 'current_holding.txt'];
 file_trade = [path_account 'trade_holding.txt'];
-file_today = [path_account 'trade_p0.txt'];
 
 %% load target file
 if exist(file_target, 'file')
@@ -33,10 +33,10 @@ else
 end
 
 %% generate trade vol, and write into trade file
-unionTicker = union(tHolding(:,1), cHolding(1));
+unionTicker = union(tHolding(:,1), cHolding(:,1));
 unionTicker(all(unionTicker == 0, 2), :) = [];
 numOfTicker = size(unionTicker,1);
-unionHolding = zeros(numOfTicker, 3);%第一列是ticker，第二列是target，第三列是current
+unionHolding = zeros(numOfTicker, 4);%第一列是ticker，第二列是target，第三列是current, 第四列是available
 unionHolding(:,1) = unionTicker;
 for i = 1:numOfTicker
     pT = find(tHolding(:,1) == unionHolding(i,1), 1, 'first');
@@ -48,56 +48,94 @@ for i = 1:numOfTicker
     end
     if isempty(pC)
         unionHolding(i,3) = 0;
+		unionHolding(i,4) = 0;
     else
         unionHolding(i,3) = cHolding(pC, 2);
+		unionHolding(i,4) = cHolding(pC, 3);
     end
 end
 diffHolding = [unionHolding(:,1) unionHolding(:,2) - unionHolding(:,3)];
 
-[idate, itime] = GetDateTimeNum();
-fprintf(fid_log, '--->>> %s_%s,\tWrite trade vol file. file = %s.\n', num2str(idate), num2str(itime), file_trade);
 fid = fopen(file_trade, 'w');
 fprintf(fid, [repmat('%15d\t',1,size(diffHolding,2)), '\n'], diffHolding');
 fclose(fid);
+[idate, itime] = GetDateTimeNum();
+fprintf(fid_log, '--->>> %s_%s,\tDONE. Write trade vol file. file = %s.\n', num2str(idate), num2str(itime), file_trade);
 
 dst_file_trade = [path_account '\HistoricalTrade\trade_holding_' num2str(idate) '_' num2str(itime) '.txt'];
 CopyFile2HistoryDir(file_trade, dst_file_trade);
 
-
 %% write into trade files for client
+% consider the available holdingunionHolding(all(diffHolding(:,2) == 0, 2), :) = [];
+unionHolding(all(diffHolding(:,2) == 0, 2), :) = [];
 diffHolding(all(diffHolding(:,2) == 0,2), :) = [];
+diffHolding(:,2) = min(abs(diffHolding(:,2)), unionHolding(:,3)) .* abs(diffHolding(:,2)) ./ diffHolding(:,2) .* (diffHolding(:,2) < 0)...
+                   + diffHolding(:,2) .* (diffHolding(:,2) > 0);
+diffHolding(all(diffHolding(:,2) == 0,2), :) = [];
+
+% devide into N_PART
 numOfTrade = size(diffHolding,1);
-fid = fopen(file_today, 'w');
-if fid > 0
-    PriceType = 'ANY';
-    Price = '0';
-    for i = 1:numOfTrade
-        if diffHolding(i,1) < 600000
-            Market = '1';
-        else
-            Market = '0';
-        end
-        Ticker = num2str(diffHolding(i,1), '%06d');
-        if diffHolding(i,2) > 0
-            BS = 'B';
-        elseif diffHolding(i,2) < 0
-            BS = 'S';
-        end
-        Vol = num2str(abs(diffHolding(i,2)));
-        
-        lines = [Market '|' Ticker '|' BS '|' '|' PriceType '|' Price '|' Vol '\n'];
-        fprintf(fid, lines);
-    end
-    fclose(fid);
-    
-    [idate, itime] = GetDateTimeNum();
-    fprintf(fid_log, '--->>> %s_%s,\tDone write trade file. file = %s.\n', num2str(idate), num2str(itime), file_today);
-    dst_file_today = [path_account '\HistoricalTrade\trade_p0_' num2str(idate) '_' num2str(itime) '.txt'];
-    CopyFile2HistoryDir(file_today, dst_file_today);   
-else
-    fprintf(fid_log, '--->>> %s_%s,\tError when write trade file. file = %s.\n', num2str(idate), num2str(itime), file_today);
-    fclose(fid);
-    return;
+one = ones(numOfTrade, numOfTrade);
+
+dev_vol = floor(abs(diffHolding(:,2)) / 100 / N_PART);
+rem_vol = rem(floor(abs(diffHolding(:,2) / 100)), N_PART);
+
+dev_vol = diag(dev_vol);
+dev_vol = (one * dev_vol)';
+dev_vol(:,N_PART+1:end) = [];
+
+rem_vol = diag(rem_vol);
+rem_vol = (one * rem_vol)';
+rem_vol(:, N_PART+1:end) = [];
+tmp = 1:N_PART;
+tmp = repmat(tmp, numOfTrade, 1);
+tmp(:, N_PART+1:end) = [];
+rem_vol = ((rem_vol - tmp) >= 0);
+
+bs = abs(diffHolding(:,2)) ./ diffHolding(:,2);
+bs = diag(bs);
+bs = (one * bs)';
+bs(:,N_PART+1:end) = [];
+
+child_vol = (dev_vol + rem_vol) . * bs * 100; % 乘以100后变成股数, 并且带有符号
+
+% begin to write in parts
+for ipart = 1:N_PART
+	file_name = ['trade_p' num2str(ipart)];
+	file_today = [path_account file_name '.txt'];
+	
+	fid = fopen(file_today, 'w');
+	if fid > 0
+		PriceType = 'ANY';
+		Price = '0';
+		for i = 1:numOfTrade
+			if diffHolding(i,1) < 600000
+				Market = '1';
+			else
+				Market = '0';
+			end
+			Ticker = num2str(diffHolding(i,1), '%06d');
+			if child_vol(i,ipart) > 0
+				BS = 'B';
+			elseif child_vol(i,ipart) < 0
+				BS = 'S';
+			end
+			Vol = num2str(abs(child_vol(i,ipart)));
+			
+			lines = [Market '|' Ticker '|' BS '|' '|' PriceType '|' Price '|' Vol '\n'];
+			fprintf(fid, lines);
+		end
+		fclose(fid);
+		
+		[idate, itime] = GetDateTimeNum();
+		fprintf(fid_log, '--->>> %s_%s,\tDone write trade file. file = %s.\n', num2str(idate), num2str(itime), file_today);
+		dst_file_today = [path_account '\HistoricalTrade\' file_name '_' num2str(idate) '_' num2str(itime) '.txt'];
+		CopyFile2HistoryDir(file_today, dst_file_today);   
+	else
+		fprintf(fid_log, '--->>> %s_%s,\tError when write trade file. file = %s.\n', num2str(idate), num2str(itime), file_today);
+		fclose(fid);
+		return;
+	end
 end
 
 [idate, itime] = GetDateTimeNum();
