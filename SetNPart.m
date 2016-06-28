a67f5cd549027039d13a02ca2a81df31552ca84f
qiyujian <qiyujian2010@live.com>
2016-06-28 18:39:35 +0800

    debug SetNPart.m

--- SetNPart.m
+++ SetNPart.m
@@ -1,4 +1,4 @@
 function account_info = SetNPart(mAccountInfo, j_id, sell_size, buy_size)
-npart = floor(max(abs(sell_size), abs(buy_size)) / 2e6);
+npart = ceil(max(abs(sell_size), abs(buy_size)) / 2e6);
 mAccountInfo{j_id}.NPART = num2str(npart);
 account_info = mAccountInfo;
